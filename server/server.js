41e7b85d233cdd761c4f4fc9f9f6fbe3af5e48b9
Soobin Kang <SoobinK@multotec.au>
2018-06-29 12:39:23 +1000

    Test the delete todos/:id route

--- server/server.js
+++ server/server.js
@@ -30,7 +30,22 @@ app.get('/todos', (req,res)=>{
     res.send({todos});
   }, (e) =>{
     res.status(400).send(e);
-  })
+  });
+});
+
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });  
 });
 
 //GET /todos/dfadsfs (v 78)

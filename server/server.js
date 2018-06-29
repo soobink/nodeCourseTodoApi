ed49a9c43dd2ee93f23461d9e864ecb2f7e6ab70
Soobin Kang <SoobinK@multotec.au>
2018-06-29 13:30:22 +1000

    ADD PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
-
-var express = require ('express');
-var bodyParser = require('body-parser');
+const express = require ('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -45,7 +45,7 @@ app.get('/todos/:id',(req,res)=>{
     res.send({todo});
   }).catch((e)=>{
     res.status(400).send();
-  });  
+  });
 });
 
 //GET /todos/dfadsfs (v 78)
@@ -104,6 +104,30 @@ app.delete('/todos/:id', (req, res)=>{
     //error
       //400 with empty body
 
+app.patch('/todos/:id', (req,res)=>{
+  var id = req.params.id;
+  var body =_.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else{
+    body.completedAt = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+});
 
 app.listen(port, ()=>{
   console.log(`Started up at port ${port}`);
